80f2d3878d07af226733e4f5372d450571e232fb
CashALot <nikitagoncharovmtu@yandex.ru>
2017-05-23 09:50:06 +0300

    Mondo addition

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 require('dotenv').config()
 var bb = require('bot-brother');
+var mongoClient = require("mongodb").MongoClient;
 var bot = bb({
   key: process.env.TG_BOT_API_KEY,
   sessionManager: bb.sessionManager.memory(),
   polling: { interval: 0, timeout: 1 }
 });
-var dialogger = require('./bot_parts/dialogger')
-var CONTEXT = ''
-const ng_id = process.env.NG_ID
+var dialogger   = require('./bot_parts/dialogger')
+var CONTEXT     = ''
+const ng_id     = process.env.NG_ID
+const mongo_url = process.env.MONGOD_URL
+
+mongoClient.connect(mongo_url, function(err, db){
+
+    if(err){
+        return console.log(err);
+    } else {
+      console.log(`DB is started :: ${db.databaseName}`);
+    }
+    var usersCollection = db.collection("users")
+    var user_nikita     = { name: "Nikita", lastName: "Goncharov", _id: process.env.NG_ID }
+    usersCollection.insertOne(user_nikita, (err, result) => {
+      if(err) {
+        return console.log(`ERR :: ${err}`);
+      } else {
+        console.log(`Result :: ${result.ops}`);
+        db.close()
+      }
+    })
+})
 
 bot.use('before', bb.middlewares.typing());
 bot.use('before', bb.middlewares.botanio(process.env.METRICS_KEY));
@@ -25,6 +46,5 @@ bot.command('upload_photo')
   return ctx.sendMessage('Drop me a photo, please');
 })
 .answer(function (ctx) {
-
   return ctx.sendPhoto(ctx.message.photo[0].file_id, {caption: 'I got your photo!'});
-});
+})
